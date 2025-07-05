b1d57bd4487954fc57c0bf3de133d2f990ac6ce6
Prensa <veiprensa@gmail.com>
2025-07-04 21:27:53 -0300

    atualizações em world job

---
+++ world_job/script.js
@@ -0,0 +1,11 @@
+const man = "man";
+console.log (man);
+const woman = "girgl";
+console.log (woman);
+const zoo = {
+    cat : "Fish",
+    dog : "Bird",
+    fish : "Dog"
+}
+console.log(zoo); // Fish
+
